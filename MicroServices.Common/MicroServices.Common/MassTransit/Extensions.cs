64bb69a5063a8031e766ef95389ba9c55ff79682
Ankit Jasuja <jasujaankit91@gmail.com>
2023-01-16 14:38:02 +0530

    Added implementation to configure RabbitMQ to common project that was missing

---
+++ MicroServices.Common/MicroServices.Common/MassTransit/Extensions.cs
@@ -0,0 +1,32 @@
+﻿using MassTransit;
+using MassTransit.Definition;
+using MicroServices.Common.Settings;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace MicroServices.Common.MassTransit
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddMassTransitWithRabbitMq(this IServiceCollection services)
+        {
+            services.AddMassTransit(configure=> 
+            {
+                configure.AddConsumers(Assembly.GetEntryAssembly());
+                configure.UsingRabbitMq((context,configurator) => 
+                {
+                    var service = context.GetService<IConfiguration>();
+                    var rabbitMQSettings = service?.GetSection("RabbitMQSettings").Get<RabbitMQSettings>();
+                    var serviceSettings = service?.GetSection("ServiceSettings").Get<ServiceSettings>();
+                    configurator.Host(rabbitMQSettings?.Host);
+                    configurator.ConfigureEndpoints(context, 
+                        new KebabCaseEndpointNameFormatter(serviceSettings?.ServiceName,
+                        includeNamespace: false));
+                });
+            });
+            services.AddMassTransitHostedService();
+            return services;
+        }
+    }
+}
